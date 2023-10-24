d825fbf4141e2fa7bc3c26a463ff9cb597dc2a32
samstutzman7 <sam.stutz7@hotmail.com>
2023-10-24 12:03:18 -0500

    Work Experience Section Completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -64,6 +64,8 @@ const ComputersCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
+          enablePan={false}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
